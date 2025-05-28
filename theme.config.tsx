7a0e39d96ffdad91b163a58612232effddb5e5c2
kiselandrey <undrokisel@gmail.com>
2025-05-29 01:34:43 +0500

    final

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>Твоя шаурма - пользовательская документация</span>,
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Создано А.В. Киселем, ОНИКС, 2025',
   },
+  search: {
+    component: null
+  },
+  feedback: {content: null},
+  editLink: {component: null},
+  toc: {title: "На этой странице"},
+  gitTimestamp: ''
 }
 
 export default config
